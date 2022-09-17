c436eb0d569a9634687dbd850ab0beee340a3821
nattygirma <nattygirma28@gmail.com>
2022-09-17 06:19:42 +0530

    Initial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ class Home extends React.Component {
        <div>
 
         <div
-          class="hero-full-container background-image-container white-text-container background">
+          class="hero-full-container background-image-container white-text-container backgroundhome">
           <div class="container">
             <div class="row">
               <div class="col-xs-12">
